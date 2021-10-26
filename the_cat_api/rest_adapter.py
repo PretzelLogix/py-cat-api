56a8003fbd1df46ff651dc6a41012201dee7bc1c
PretzelLogix <pretzellogix@github.com>
2021-10-25 21:05:41 -0400

    Refactored rest_adapter.py -- Created _do() private method + get(), post(), and delete() public methods

--- the_cat_api/rest_adapter.py
+++ the_cat_api/rest_adapter.py
@@ -4,27 +4,27 @@ from typing import List, Dict
 
 class RestAdapter:
     def __init__(self, hostname: str, api_key: str = '', ver: str = 'v1', ssl_verify: bool = True):
-        self.url = "https://{}/{}/".format(hostname, ver)
+        self.url = f"https://{hostname}/{ver}/"
         self._api_key = api_key
         self._ssl_verify = ssl_verify
         if not ssl_verify:
             # noinspection PyUnresolvedReferences
             requests.packages.urllib3.disable_warnings()
 
-    def get(self, endpoint: str) -> List[Dict]:
+    def _do(self, http_method: str, endpoint: str, ep_params: Dict = None, data: Dict = None):
         full_url = self.url + endpoint
         headers = {'x-api-key': self._api_key}
-        response = requests.get(url=full_url, verify=self._ssl_verify, headers=headers)
+        response = requests.request(method=http_method, url=full_url, verify=self._ssl_verify, headers=headers, params=ep_params, json=data)
         data_out = response.json()
-        if response.status_code >= 200 and response.status_code <= 299:     # OK
+        if response.status_code >= 200 and response.status_code <= 299:     # 200 to 299 is OK
             return data_out
-        raise Exception(data_out["message"])    # Todo: raise custom exception later
+        raise Exception(data_out["message"])
+
+    def get(self, endpoint: str, ep_params: Dict = None) -> List[Dict]:
+        return self._do(http_method='GET', endpoint=endpoint, ep_params=ep_params)
 
     def post(self, endpoint: str, ep_params: Dict = None, data: Dict = None):
-        full_url = self.url + endpoint
-        headers = {'x-api-key': self._api_key}
-        response = requests.post(url=full_url, verify=self._ssl_verify, headers=headers, params=ep_params, json=data)
-        data_out = response.json()
-        if response.status_code >= 200 and response.status_code <= 299:     # OK
-            return
-        raise Exception(data_out["message"])
+        return self._do(http_method='POST', endpoint=endpoint, ep_params=ep_params, data=data)
+
+    def delete(self, endpoint: str, ep_params: Dict = None, data: Dict = None):
+        return self._do(http_method='DELETE', endpoint=endpoint, ep_params=ep_params, data=data)

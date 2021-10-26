7ac836aa6c84307c14a361400295a09152e72987
PretzelLogix <pretzellogix@github.com>
2021-10-25 20:44:46 -0400

    Added the_cat_api module and initial rest_adapter.py

---
+++ the_cat_api/rest_adapter.py
@@ -0,0 +1,30 @@
+import requests
+from typing import List, Dict
+
+
+class RestAdapter:
+    def __init__(self, hostname: str, api_key: str = '', ver: str = 'v1', ssl_verify: bool = True):
+        self.url = "https://{}/{}/".format(hostname, ver)
+        self._api_key = api_key
+        self._ssl_verify = ssl_verify
+        if not ssl_verify:
+            # noinspection PyUnresolvedReferences
+            requests.packages.urllib3.disable_warnings()
+
+    def get(self, endpoint: str) -> List[Dict]:
+        full_url = self.url + endpoint
+        headers = {'x-api-key': self._api_key}
+        response = requests.get(url=full_url, verify=self._ssl_verify, headers=headers)
+        data_out = response.json()
+        if response.status_code >= 200 and response.status_code <= 299:     # OK
+            return data_out
+        raise Exception(data_out["message"])    # Todo: raise custom exception later
+
+    def post(self, endpoint: str, ep_params: Dict = None, data: Dict = None):
+        full_url = self.url + endpoint
+        headers = {'x-api-key': self._api_key}
+        response = requests.post(url=full_url, verify=self._ssl_verify, headers=headers, params=ep_params, json=data)
+        data_out = response.json()
+        if response.status_code >= 200 and response.status_code <= 299:     # OK
+            return
+        raise Exception(data_out["message"])

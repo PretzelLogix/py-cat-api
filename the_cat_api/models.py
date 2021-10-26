5b4f3b046bd9cf8d6bc816aeaed2291eb4ff858e
PretzelLogix <pretzellogix@github.com>
2021-10-25 22:26:45 -0400

    Added more strong data models

--- the_cat_api/models.py
+++ the_cat_api/models.py
@@ -1,4 +1,4 @@
-from typing import List, Dict
+from typing import List, Dict, Union
 
 
 class Result:
@@ -14,3 +14,41 @@ class Result:
         self.status_code = int(status_code)
         self.message = str(message)
         self.data = data if data else []
+
+
+class Fact:
+    def __init__(self, id: str, text: str, language_code: str, breed_id: str):
+        self.id = id
+        self.text = text
+        self.language_code = language_code
+        self.breed_id = breed_id
+
+
+class Weight:
+    def __init__(self, imperial: str, metric: str):
+        self.imperial = imperial
+        self.metric = metric
+
+
+class Category:
+    def __init__(self, id: int, name: str):
+        self.id = id
+        self.name = name
+
+
+class Breed:
+    def __init__(self, weight: Union[Weight, dict], id: str, name: str, country_codes: str, country_code: str,
+                 description: str, temperament: str = '', origin: str = '', life_span: str = '', alt_names: str = '',
+                 wikipedia_url: str = '', **kwargs) -> None:
+        self.weight = Weight(**weight) if isinstance(weight, dict) else weight
+        self.id = id
+        self.name = name
+        self.origin = origin
+        self.country_codes = country_codes
+        self.country_code = country_code
+        self.description = description
+        self.temperament = temperament
+        self.life_span = life_span
+        self.alt_names = alt_names
+        self.wikipedia_url = wikipedia_url
+        self.__dict__.update(kwargs)

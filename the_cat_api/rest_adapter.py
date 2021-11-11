344356943584088b8a53bff76f5e66cab27feaa1
PretzelLogix <pretzellogix@github.com>
2021-11-10 22:03:06 -0500

    sync'd up code with text

--- the_cat_api/rest_adapter.py
+++ the_cat_api/rest_adapter.py
@@ -14,27 +14,24 @@ class RestAdapter:
             # noinspection PyUnresolvedReferences
             requests.packages.urllib3.disable_warnings()
 
-    def _do(self, http_method: str, endpoint: str, ep_params: Dict = None, data: Dict = None) -> Result:
+    def _do(self, http_method: str, endpoint: str, ep_params: Dict = None, data: Dict = None):
         full_url = self.url + endpoint
         headers = {'x-api-key': self._api_key}
         try:
-            response = requests.request(method=http_method, url=full_url, verify=self._ssl_verify, headers=headers, params=ep_params, json=data)
+            response = requests.request(method=http_method, url=full_url, verify=self._ssl_verify,
+                                        headers=headers, params=ep_params, json=data)
         except requests.exceptions.RequestException as e:
-            raise TheCatApiException(str(e)) from e
-        try:
-            data_out = response.json()
-        except (ValueError, JSONDecodeError) as e:
-            return Result(False, response.status_code, message=str(e))
+            raise TheCatApiException("Request failed") from e
+        data_out = response.json()
         if response.status_code >= 200 and response.status_code <= 299:     # 200 to 299 is OK
-            return Result(True, response.status_code, message=response.reason, data=data_out)
-        else:
-            return Result(False, response.status_code, message=response.reason, data=data_out)
+            return data_out
+        raise Exception(data_out["message"])
 
-    def get(self, endpoint: str, ep_params: Dict = None) -> Result:
+    def get(self, endpoint: str, ep_params: Dict = None) -> List[Dict]:
         return self._do(http_method='GET', endpoint=endpoint, ep_params=ep_params)
 
-    def post(self, endpoint: str, ep_params: Dict = None, data: Dict = None) -> Result:
+    def post(self, endpoint: str, ep_params: Dict = None, data: Dict = None) -> List[Dict]:
         return self._do(http_method='POST', endpoint=endpoint, ep_params=ep_params, data=data)
 
-    def delete(self, endpoint: str, ep_params: Dict = None, data: Dict = None) -> Result:
+    def delete(self, endpoint: str, ep_params: Dict = None, data: Dict = None) -> List[Dict]:
         return self._do(http_method='DELETE', endpoint=endpoint, ep_params=ep_params, data=data)

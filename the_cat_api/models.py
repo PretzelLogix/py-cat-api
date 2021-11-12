5aad56fec39b499f2f1a5b963358ca577dc80b78
PretzelLogix <pretzellogix@github.com>
2021-11-11 21:47:48 -0500

    Added Result model

---
+++ the_cat_api/models.py
@@ -0,0 +1,8 @@
+from typing import List, Dict
+
+
+class Result:
+    def __init__(self, status_code: int, message: str = '', data: List[Dict] = None):
+        self.status_code = int(status_code)
+        self.message = str(message)
+        self.data = data if data else []

a448404ff40a63125fbe79f311bad49721181b1b
PretzelLogix <pretzellogix@github.com>
2021-11-11 23:17:02 -0500

    Revised the code for Step 7: Add Comments

--- the_cat_api/models.py
+++ the_cat_api/models.py
@@ -3,6 +3,12 @@ from typing import List, Dict
 
 class Result:
     def __init__(self, status_code: int, message: str = '', data: List[Dict] = None):
+        """
+        Result data model
+        :param status_code: HTTP Status Code
+        :param message: General message about the result
+        :param data: a List of Dictionaries
+        """
         self.status_code = int(status_code)
         self.message = str(message)
         self.data = data if data else []
--- the_cat_api/rest_adapter.py
+++ the_cat_api/rest_adapter.py
@@ -7,7 +7,15 @@ from the_cat_api.models import Result
 
 
 class RestAdapter:
-    def __init__(self, hostname: str, api_key: str = '', ver: str = 'v1', ssl_verify: bool = True, logger: logging.Logger = None):
+    def __init__(self, hostname: str = 'api.thecatapi.com', api_key: str = '', ver: str = 'v1', ssl_verify: bool = True, logger: logging.Logger = None):
+        """
+        Constructor for RestAdapter
+        :param hostname: Normally, api.thecatapi.com
+        :param api_key: (optional) string used for authentication when POSTing or DELETEing
+        :param ver: always v1
+        :param ssl_verify: Normally set to True, but if having SSL/TLS cert validation issues, can turn off with False
+        :param logger: (optional) If your app has a logger, pass it in here.
+        """
         self._logger = logger or logging.getLogger(__name__)
         self.url = f"https://{hostname}/{ver}/"
         self._api_key = api_key
@@ -17,10 +25,20 @@ class RestAdapter:
             requests.packages.urllib3.disable_warnings()
 
     def _do(self, http_method: str, endpoint: str, ep_params: Dict = None, data: Dict = None) -> Result:
+        """
+        Private method for get(), post(), delete(), etc. methods
+        :param http_method: GET, POST, DELETE, etc.
+        :param endpoint: URL Endpoint as a string
+        :param ep_params: Dictionary of Endpoint parameters (Optional)
+        :param data: Dictionary of data to pass to TheCatApi (Optional)
+        :return: a Result object
+        """
         full_url = self.url + endpoint
         headers = {'x-api-key': self._api_key}
         log_line_pre = f"method={http_method}, url={full_url}, params={ep_params}"
         log_line_post = ', '.join((log_line_pre, "success={}, status_code={}, message={}"))
+
+        # Log HTTP params and perform an HTTP request, catching and re-raising any exceptions
         try:
             self._logger.debug(msg=log_line_pre)
             response = requests.request(method=http_method, url=full_url, verify=self._ssl_verify,
@@ -28,11 +46,15 @@ class RestAdapter:
         except requests.exceptions.RequestException as e:
             self._logger.error(msg=(str(e)))
             raise TheCatApiException("Request failed") from e
+
+        # Deserialize JSON output to Python object, or return failed Result on exception
         try:
             data_out = response.json()
         except (ValueError, JSONDecodeError) as e:
             self._logger.error(msg=log_line_post.format(False, None, e))
             raise TheCatApiException("Bad JSON in response") from e
+
+        # If status_code in 200-299 range, return success Result with data, otherwise raise exception
         is_success = 299 >= response.status_code >= 200     # 200 to 299 is OK
         log_line = log_line_post.format(is_success, response.status_code, response.reason)
         if is_success:

ca4b5e6e3ff082e5d64da4860b98417bcf60d62a
PretzelLogix <pretzellogix@github.com>
2021-11-11 23:18:32 -0500

    Changed a comment

--- the_cat_api/models.py
+++ the_cat_api/models.py
@@ -4,10 +4,10 @@ from typing import List, Dict
 class Result:
     def __init__(self, status_code: int, message: str = '', data: List[Dict] = None):
         """
-        Result data model
-        :param status_code: HTTP Status Code
-        :param message: General message about the result
-        :param data: a List of Dictionaries
+        Result returned from low-level RestAdapter
+        :param status_code: Standard HTTP Status code
+        :param message: Human readable result
+        :param data: Python List of Dictionaries (or maybe just a single Dictionary on error)
         """
         self.status_code = int(status_code)
         self.message = str(message)
